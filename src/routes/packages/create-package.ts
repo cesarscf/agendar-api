a65d23f28079f4505ebce40ba0984fed12fa507c
Cesar <cesarscfilho@gmail.com>
2025-07-17 19:36:07 -0300

    feat: add package return after create

--- src/routes/packages/create-package.ts
+++ src/routes/packages/create-package.ts
@@ -1,41 +1,57 @@
-import { db } from "@/db"
-import { packages } from "@/db/schema"
-import { auth } from "@/middlewares/auth"
-import { establishmentHeaderSchema } from "@/utils/schemas/headers"
-import { packageSchema } from "@/utils/schemas/packages"
-import type { FastifyInstance } from "fastify"
-import type { ZodTypeProvider } from "fastify-type-provider-zod"
-import z from "zod"
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { db } from "@/db";
+import { packages } from "@/db/schema";
+import { auth } from "@/middlewares/auth";
+import { establishmentHeaderSchema } from "@/utils/schemas/headers";
+import { packageSchema } from "@/utils/schemas/packages";
+import { BadRequestError } from "../erros/bad-request-error";
 
 export async function createPackage(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .register(auth)
-    .post(
-      "/packages",
-      {
-        schema: {
-          tags: ["Package"],
-          summary: "Create package",
-          security: [{ bearerAuth: [] }],
-          headers: establishmentHeaderSchema,
-          body: packageSchema.omit({ id: true }),
-          response: {
-            204: z.null(),
-          },
-        },
-      },
-      async (request, reply) => {
-        const { establishmentId } = await request.getCurrentEstablishmentId()
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.post(
+			"/packages",
+			{
+				schema: {
+					tags: ["Package"],
+					summary: "Create package",
+					security: [{ bearerAuth: [] }],
+					headers: establishmentHeaderSchema,
+					body: packageSchema.omit({ id: true }),
+					response: {
+						204: packageSchema,
+					},
+				},
+			},
+			async (request, reply) => {
+				const { establishmentId } = await request.getCurrentEstablishmentId();
 
-        const data = request.body
+				const data = request.body;
 
-        await db.insert(packages).values({
-          ...data,
-          establishmentId,
-        })
+				const [createdPackage] = await db
+					.insert(packages)
+					.values({
+						...data,
+						establishmentId,
+					})
+					.returning({
+						id: packages.id,
+						name: packages.name,
+						active: packages.active,
+						commission: packages.commission,
+						description: packages.description,
+						image: packages.image,
+						price: packages.price,
+					});
 
-        return reply.status(204).send()
-      }
-    )
+				if (!createdPackage) {
+					throw new BadRequestError("Failed to create package");
+				}
+
+				return reply.status(204).send(createdPackage);
+			},
+		);
 }
